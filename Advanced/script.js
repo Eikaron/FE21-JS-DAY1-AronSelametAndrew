9189f71f03740d104d6f582b92d9a15be2a76535
Aron Faulhaber <aron.faulhaber@gmail.com>
2021-09-22 10:34:39 +0200

    advanced

---
+++ Advanced/script.js
@@ -0,0 +1,25 @@
+//Exercise 1
+
+var a = 7;
+var b = 14;
+var c="21";
+var d = '35';
+var e = 42;
+
+var sum = a+b+ Number(c)+Number(d)+e;
+//console.log(sum);
+
+//Exercise 2
+var numbers = ['1', 15, 8, "1"];
+
+var multi = Number(numbers[0]*numbers[1]*numbers[2]*Number(numbers[3]));
+//console.log(multi);
+
+document.write(sum/multi);
+
+
+//Exercise 2
+
+var myArray = [ [1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 25, 7], [21, 4, 6, 17], [3, 5, 26, 3] ];
+console.log(myArray[1][1], myArray[4][2], myArray[5][3], myArray[2][3], myArray[2][2])
+
