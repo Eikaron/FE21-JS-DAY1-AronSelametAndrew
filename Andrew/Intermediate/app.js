d4966b56f3c23646f72e9418c2232093484b4eb1
Andi Gorgoyle <fourchestra@tutanota.com>
2021-09-22 13:08:59 +0200

    print fruit list to DOM

--- Andrew/Intermediate/app.js
+++ Andrew/Intermediate/app.js
@@ -26,3 +26,14 @@ console.log(temp);
 for (index in temp) {
     console.log(temp[index]);
 }
+
+document.getElementById('fruit').innerHTML = temp.join(", ");
+document.getElementById('fruit2').innerHTML =
+    temp[0] + ",<br>" +
+    temp[1] + ",<br>" +
+    temp[2] + ",<br>" +
+    temp[3] + ",<br>" +
+    temp[4] + ",<br>" +
+    temp[5] + ",<br>" +
+    temp[6] + ",<br>" +
+    temp[7] + ".<br>";
